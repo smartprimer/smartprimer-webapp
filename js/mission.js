a1c2b95fc3b083c12d8cb08d2a82c3e876de41ee
“sandhini” <sandhini.agarwal19@gmail.comssh-keygen -t rsa -C sandhini.agarwal19@gmail.com>
2017-07-18 12:06:22 -0700

    changed mission1

--- js/mission.js
+++ js/mission.js
@@ -1,12 +1,51 @@
 class mission{
   constructor(){
     this.revealButton = document.querySelector("#helpButton");
+
+    this.heightButton = document.querySelector("#inputHeight");
+    this.lengthButton = document.querySelector("#inputLength");
+    this.ansButton = document.querySelector("#inputAns");
+
+    this.showNextQues = this.showNextQues.bind(this);
+
+    this.heightButton.addEventListener('click', this.showNextQues);
+    this.lengthButton.addEventListener('click', this.showNextQues);
+    this.ansButton.addEventListener('click', this.showNextQues);
+
+
     this.revealStep = this.revealStep.bind(this);
     this.numClick = 0;
     this.showStep = this.showStep.bind(this);
 
 
     this.revealButton.addEventListener('click', this.revealStep);
+  }
+
+  showNextQues(event){
+    console.log("oijoij");
+    event.preventDefault();
+    console.log(this.lengthButton);
+    const button = event.currentTarget;
+    console.log(button);
+    let toHide = null;
+    let toShow = null;
+    if (button === this.lengthButton){
+       toHide = document.querySelector("#answerBox1");
+       toShow = document.querySelector("#answerBox2");
+
+    }
+    if (button === this.heightButton){
+        toHide = document.querySelector("#answerBox2");
+        toShow = document.querySelector("#answerBox3");
+    }
+    if (button === this.ansButton){
+        toHide = document.querySelector("#answerBox3");
+        toShow = document.querySelector("#answerBox4");
+        const finalAns = document.querySelector("#finalAns");
+        finalAns.classList.remove('inactive');
+    }
+    toHide.classList.add("inactive");
+    toShow.classList.remove('inactive');
   }
 
   revealStep(event){
