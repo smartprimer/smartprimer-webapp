018f5f68438bb731e1683ce410815a6af041f2b1
Kat <katg@cs.stanford.edu>
2018-02-19 15:08:46 -0800

    Update communication code to mesh with new changes. Bot now responds automatically to kid's proposed answer.

--- CandyMan_js/mission.js
+++ CandyMan_js/mission.js
@@ -26,23 +26,55 @@ class mission{
       }
     }
 
-// Code that automatically transitions to next page if answer is correct
+    /**
+     * When kid inputs an answer, update the page visually to indicate if answer
+     * is correct and send both that answer and the box dimensions
+     * to the parent for evaluation.
+     */
     this.checkAnswerButton.onclick = function(){
+      // Extract entered values
       const height = document.querySelector('#Height_Answer');
       const width = document.querySelector('#Width_Answer');
       const depth = document.querySelector('#Depth_Answer');
       const ans = document.querySelector("#Final_Answer");
+
+      // Use height, width, and depth to determine correct number of candies
       let correct_ans = (parseInt(width.value, 10) * parseInt(height.value, 10) * parseInt(depth.value, 10))/2;
-      if (correct_ans == ans.value){
+      
+      // Extract kid's proposed answer. Default to 0.
+      let proposed_ans = ans.value;
+      if (proposed_ans === "") { // If user didn't submit answer, default to 0
+        proposed_ans = 0;
+      }
+      
+      // Respond visually to proposed answer
+      if (correct_ans == proposed_ans){
         toggleModal();
       } else {
         document.querySelector("#try_again").classList.add("inactive");
         setTimeout(function() {  document.querySelector("#try_again").classList.remove("inactive");}, 200);
       }
-    }
+
+      // Send information about proposed answer to parent
+      sendToParent(encodeMsg("mission1Answer", correct_ans, proposed_ans));
+    };
+
+    /**
+     * Listen for the parent's response to the proposed answer and respond
+     * accordingly. Right now, this doesn't do anything tangible.
+     */
+    bindEvent(window, 'message', function (e) {
+      let msg = decodeMsg(e.data);
+      if (msg[0] !== "mission1") return;
+      let outcome = msg[1];
+      if (outcome === "correct") {
+        // TODO: Add gif. When kid closes gif, invoke the following redirect.
+        // window.location.href = "4_after_count_candies.html";
+      } else if (outcome === "double" || outcome === "incorrect") {
+        // TODO: Visual indication for incorrect answer
+      }
+    });
   }
-
-
 
 }
 
