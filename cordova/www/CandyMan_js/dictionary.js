4ba9287de5ce06902f27168d51c55d77679714b5
sherry <sherry.s.ruan@gmail.com>
2018-03-06 16:08:05 -0800

    move the files to the right folder (cordova/www)

--- cordova/www/CandyMan_js/dictionary.js
+++
@@ -1,3 +0,0 @@
-
-const dictionaryRet = [{"name":"diameter", "meaning": "The diameter of a coin is a straight line passing from side to side through the center"},
-{"name":"blue words", "meaning": "Exactly! You would usually find definitions here. By the way, speaking of blue, did you know that blue is associated with calm and comfort? That is why most social media websites use blue."}];
